075e7b58257aa5be6fbd2a96ab7d0918e81d677e
scottwarburton <scott_warburton@hotmail.com>
2021-08-09 16:41:25 +1000

    purpose

--- Python project begin.py
+++ Python project begin.py
@@ -1,6 +1,20 @@
 
-#Begin python project here
+#Finance project - Flask, python, SQL, javascript
 
-#Finance project - python and SQL
+#stock portfolio analyser
+"""
+Analyse
+enter ticker to analyse
+request current data from yahoo finance
+display stock statistics and matplotlib chart
 
-#decide purpose
+Amend Portfolio
+click add to portfolio
+enter buy price, and number of units
+save info to sqlite - portfolio table
+
+View Portfolio
+display portfolio summary
+"""
+
+
