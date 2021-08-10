b4e86a1dfa7780d654afa03de13363193fe33540
scottwarburton <scott_warburton@hotmail.com>
2021-08-10 16:16:27 +1000

    update

--- begin.py
+++ begin.py
@@ -2,8 +2,23 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-begin = Flask(__name__)
+begin = Flask(__name
+begin.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flaskdb.db"
+db = SQLAlchemy(begin)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
+        #when creating a task, will return Task then id of task created
+
+
 
 #setup route so don't end up with 404 when searching url
 
