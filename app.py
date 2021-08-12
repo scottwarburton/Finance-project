a306a18525454f164e95a076e3d64d2feb5a87dc
scottwarburton <scott_warburton@hotmail.com>
2021-08-12 15:14:21 +1000

    update

--- app.py
+++ app.py
@@ -19,37 +19,20 @@ class Stocks(db.Model):
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == "POST":
+        new_stock = Stocks(ticker=request.form["ticker"], units=request.form["units"], price=request.form["price"])
+        try:
+            db.session.add(new_stock)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Error adding stock"
+    else:
+        portfolio_table = Stocks.query.order_by(Stocks.date_added).all()
+        return render_template("home.html", stock=portfolio_table)
     return render_template("home.html")
 """
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return "<Task %r>" % self.id
-        #when creating a task, will return Task then id of task created
-
-#setup route so don't end up with 404 when searching url
-
-@app.route("/", methods=["POST", "GET"])
-def index():
-    if request.method == "POST":
-        task_content = request.form["content"]  #create new task from input (html id)
-        new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task)    #add to our database
-            db.session.commit()
-            return redirect("/") #then redirect back to our index page
-        except:
-            return "Error adding task"
-        #when pressing submit button
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()    #ordering by newest to oldest (could do .first())
-        return render_template("index.html", tasks=tasks)   #
-        #when loading page
 
 @app.route("/delete/<int:id>")    #using id from table as unique identifier for tasks
 def delete(id):
@@ -73,6 +56,32 @@ def update(id):
             return "Error updating task"
     else:
         return render_template("update.html", task=task)
+        
+        
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return "<Task %r>" % self.id
+        #when creating a task, will return Task then id of task created
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form["content"]  #create new task from input (html id)
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)    #add to our database
+            db.session.commit()
+            return redirect("/") #then redirect back to our index page
+        except:
+            return "Error adding task"
+        #when pressing submit button
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()    #ordering by newest to oldest (could do .first())
+        return render_template("index.html", tasks=tasks)   #
+        #when loading page
 """
 
 if __name__ == "__main__":
