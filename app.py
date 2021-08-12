391dd1acf9dedcd9df7f1b2ecad02d4e00b45a04
scottwarburton <scott_warburton@hotmail.com>
2021-08-12 16:31:09 +1000

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
