c51e2fe6a0842ef83eda44d1324a27891196e0c2
scottwarburton <scott_warburton@hotmail.com>
2021-08-19 17:57:50 +1000

    update

--- static/script/slider.js
+++ static/script/slider.js
@@ -1,15 +1,13 @@
-
-const project-div = document.querySelector(".project-div"); //hero
 /*
-const wc-image = document.querySelector(".wc-image");       //hero image
-const watering-can = document.querySelector(".watering-can");   //headline
-const slider = document.querySelector(".slider");   //slider
+const project = document.querySelector(".project-div");
+const watering-can = document.querySelector(".watering-can");
+const slider = document.querySelector(".slider");
 */
+const wcimage = document.querySelector(".wc-image");
 const tl = new TimelineMax();
-
-tl.fromTo(project-div, 5, {height: "0%"}, {height: "80%"});
+tl.fromTo(wcimage, 1, {height: "0%"}, {height: "80%"});
 /*
-    .fromTo(project-div, 6, {width: 100%}, {width: 80%, ease: Power2.easeInOut})
+    .fromTo(wcimage, 6, {width: "100%"}, {width: "80%", ease: Power2.easeInOut});
     .fromTo(slider, 5, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, -6);
 */
 
