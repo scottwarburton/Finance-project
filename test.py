1e3321460d8fe918ca47949272a000a02a1f367b
scottwarburton <scott_warburton@hotmail.com>
2021-08-25 00:00:09 +1000

    update

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-
 import requests
 from bs4 import BeautifulSoup
 
 
 
-url = "https://finance.yahoo.com/quote/AAPL"
+url = "https://finance.yahoo.com/quote/TSLA"
 headers = {'User-Agent': 'Mozilla/5.0'}
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "lxml")
-stock_daily_return = soup.find("span", {"class": "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"}).text
+stock_daily_return = soup.find("div", {"class": "D(ib) Va(m) Maw(65%) Ov(h)"}).find("div").find_all("span")[1].text
+
 print(stock_daily_return)
