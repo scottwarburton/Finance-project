caefb2794d1978f2ade54fa705aa838b5fd74e8c
scottwarburton <scott_warburton@hotmail.com>
2021-09-08 08:55:59 +1000

    update

--- static/script/script.js
+++ static/script/script.js
@@ -1,17 +1,6 @@
-/*
-const pages = document.querySelectorAll(".page")
 
-pages.forEach(page => {
-    page.addEventListener("click", () => {
-        const target = document.querySelector(page.dataset.tabTarget)
-        pages.forEach(page => {
-            page.classList.remove("active")
-        })
-        page.classList.add("active")
-        target.classList.add("active")
-    })
-})
-*/
-//identify tab clicked, save as target
-//pass target as id to decorator fn
-//classList.add to page
+const { AwakeHeroku } = require("awake-heroku");
+
+AwakeHeroku.add("https://flaskprojectsw.herokuapp.com");
+
+AwakeHeroku.start();
